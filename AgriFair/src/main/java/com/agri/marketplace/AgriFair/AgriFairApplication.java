072b17a0fd632a5577ef680b3d6291a36a981275
hemant rajputt <rajput04hemant@gmail.com>
2025-09-19 00:40:06 +0530

    Initial commit

---
+++ AgriFair/src/main/java/com/agri/marketplace/AgriFair/AgriFairApplication.java
@@ -0,0 +1,13 @@
+package com.agri.marketplace.AgriFair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriFairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriFairApplication.class, args);
+	}
+
+}
---
+++ AgriFair/src/test/java/com/agri/marketplace/AgriFair/AgriFairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agri.marketplace.AgriFair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriFairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
